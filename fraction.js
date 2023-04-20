e06f4d30c10d02efd61d26ba6d4b31155b80050c
jayanthpk <115770022+jayanthpk@users.noreply.github.com>
2023-04-20 12:11:27 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,1 +1,11 @@
+compareTo(other) {
+    const fraction1Value = this.numerator * other.denominator;
+    const fraction2Value = other.numerator * this.denominator;
 
+    if (fraction1Value < fraction2Value) {
+      return -1; // Fraction 1 is smaller
+    } else if (fraction1Value > fraction2Value) {
+      return 1; // Fraction 1 is larger
+    } else {
+      return 0; // Fractions are equal
+    }
